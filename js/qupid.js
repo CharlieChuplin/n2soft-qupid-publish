0e5841a0a7dce4937ff1415c844632697c5bf8f9
DESKTOP-NPB9Q23\n2soft <pleasantckh@naver.com>
2023-08-17 17:57:46 +0900

    n2soft QUPID : 홈페이지 완료, 애니메이션 작성해야함
    [애니메이션 효과]
    - 홈페이지 접속 시 히어로 이미지의 세로라인 상->하 생성
    v - 상단 GNB에 맞춰 화면 이동
    - 전자우편 및 문서 서비스 정의 : 박스 통채로 아래에서 나타나는 애니메이션
    - 프로세스 : 고객사 기준으로 라인 생기면서 인포그래픽 생성효과
    - 기대효과 : 가운데서부터 퍼지도록 라인 강조
    - 활용사례 : 각 항목별로 이미지(좌/우에 맞게 등장)와 텍스트(하단에서 등장)
    - 주요고객사 : 현재는 다섯개 맞춰있지만 추후 추가될 경우 회전식(오른쪽에서 왼쪽으로 이동하는)

--- js/qupid.js
+++ js/qupid.js
@@ -1,13 +1,89 @@
-const menu = document.querySelector('#header');
-const menuHeight = menu.getBoundingClientRect().height;
+(function() {
+    "use strict"; // Start of use strict
 
-document.addEventListener('scroll', () => {
-    if (window.scrollY > menuHeight) {
-        menu.classList.add('scrolled');
-    } else {
-        menu.classList.remove('scrolled');
-    }
-});
+    const $menu = $('#header');
+    const menuHeight = $menu.outerHeight();
+    const $logo = $("#logo");
+    const $elementsToAnimate = $('.animate-on-scroll'); /*애니메이션*/
 
-/*#header.scrolled {background: #fff;} 스크롤 시 nav 배경 색상 변경*/
-/*#header.scrolled .nav .gnb a {color : #101010;} 스크롤 시 nav 폰트 색상 변경 */
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > menuHeight) {
+            $menu.addClass('scrolled');
+            $logo.attr('src', 'img/logo-qupid-bk.png');
+        } else {
+            $menu.removeClass('scrolled');
+            $logo.attr('src', 'img/logo-qupid.png');
+        }
+    });
+
+    // function checkElementsInView() {
+    //     $elementsToAnimate.each(function() {
+    //         const positionFromTop = $(this).offset().top - $(window).scrollTop();
+    //
+    //         if (positionFromTop <= $(window).height() && positionFromTop + $(this).height() >= 0) {
+    //             $(this).addClass('slide-up-animation');
+    //         }
+    //     });
+    // }
+    // $(document).ready(checkElementsInView);
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+
+    // Initialize and Configure Magnific Popup Lightbox Plugin
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+})(jQuery);
